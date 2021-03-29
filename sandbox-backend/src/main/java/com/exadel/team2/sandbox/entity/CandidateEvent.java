e83776f790b71de4d950392c7a23136ecbafdb57
Aliaksei Halkin <pochtax3@gmail.com>
2021-03-29 23:06:30 +0300

    the beginning

---
+++ sandbox-backend/src/main/java/com/exadel/team2/sandbox/entity/CandidateEvent.java
@@ -0,0 +1,9 @@
+package com.exadel.team2.sandbox.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CANDIDATE_EVENT")
+public class CandidateEvent {
+}
---
+++ sandbox-backend/src/main/java/com/exadel/team2/sandbox/entity/InterviewFeedback.java
@@ -0,0 +1,10 @@
+package com.exadel.team2.sandbox.entity;
+
+import javax.persistence.Table;
+import javax.persistence.Entity;
+
+@Entity
+@Table(name = "INTERVIEW_FEEDBACK")
+public class InterviewFeedback {
+
+}
