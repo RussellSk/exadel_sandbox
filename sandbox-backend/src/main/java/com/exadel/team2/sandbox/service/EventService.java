804bea820656565f434ff961bca9c3f602a1d8b8
Vadim Blagiy <vadblagiy@gmail.com>
2021-04-07 20:57:37 +0300

    attached pagination for event; fixed little bug

--- sandbox-backend/src/main/java/com/exadel/team2/sandbox/service/EventService.java
+++ sandbox-backend/src/main/java/com/exadel/team2/sandbox/service/EventService.java
@@ -3,6 +3,8 @@ package com.exadel.team2.sandbox.service;
 import com.exadel.team2.sandbox.web.EventCreateDTO;
 import com.exadel.team2.sandbox.web.EventResponseDTO;
 import com.exadel.team2.sandbox.web.EventUpdateDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -10,6 +12,7 @@ public interface EventService {
 
     EventResponseDTO getById (Long id);
     List <EventResponseDTO> getAll();
+    Page<EventResponseDTO> getAllPageable(Pageable pageable);
     EventResponseDTO save(EventCreateDTO eventCreateDTO);
     EventResponseDTO update(Long id, EventUpdateDTO eventUpdateDTO);
     void delete(Long id);
