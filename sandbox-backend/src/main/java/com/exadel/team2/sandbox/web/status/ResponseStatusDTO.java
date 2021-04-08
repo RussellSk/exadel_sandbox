ecd4148de04c927fa5e0497f10f61aafb5b19078
6uten <butenkodanil84@gmail.com>
2021-04-08 10:42:39 +0300

    Added validation to DTO.

--- sandbox-backend/src/main/java/com/exadel/team2/sandbox/web/status/ResponseStatusDTO.java
+++ sandbox-backend/src/main/java/com/exadel/team2/sandbox/web/status/ResponseStatusDTO.java
@@ -1,21 +1,27 @@
-package com.exadel.team2.sandbox.web;
+package com.exadel.team2.sandbox.web.status;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 
 @Data
 public class ResponseStatusDTO {
 
+    @NotNull
     private Long id;
 
+    @NotNull
     private String name;
 
+    @NotNull
     private String description;
 
+    @NotNull
     private LocalDateTime createdAt;
 
+    @NotNull
     private LocalDateTime updatedAt;
 
 }
