841a78042d02d86c55bb862302ff5c9ba01ad934
Vadim Blagiy <vadblagiy@gmail.com>
2021-04-15 22:57:13 +0300

    remark

--- sandbox-web/src/main/java/com/exadel/team2/sandbox/SandboxApplication.java
+++ sandbox-web/src/main/java/com/exadel/team2/sandbox/SandboxApplication.java
@@ -19,7 +19,7 @@ public class SandboxApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8085");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
