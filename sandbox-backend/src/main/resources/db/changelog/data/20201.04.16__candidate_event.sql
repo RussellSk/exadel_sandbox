ce9542a3011dd09e376947766c6a40addf96ca6a
Aliaksei Halkin <pochtax3@gmail.com>
2021-04-18 19:39:03 +0300

    formatted style code

---
+++ sandbox-backend/src/main/resources/db/changelog/data/20201.04.16__candidate_event.sql
@@ -0,0 +1,3 @@
+INSERT INTO sandbox.CANDIDATE_EVENT (sandbox.CANDIDATE_EVENT.CNEV_ID, sandbox.CANDIDATE_EVENT.CN_ID,
+                                     sandbox.CANDIDATE_EVENT.EV_ID, sandbox.CANDIDATE_EVENT.CNV_CREATED_AT) values
+(2,1,1,now());
---
+++ sandbox-backend/src/main/resources/db/changelog/data/2021.04.16_IFB.sql
@@ -0,0 +1,7 @@
+INSERT INTO sandbox.INTERVIEW_FEEDBACK(sandbox.INTERVIEW_FEEDBACK.IFB_ID,
+                                       sandbox.INTERVIEW_FEEDBACK.CN_ID,
+                                       sandbox.INTERVIEW_FEEDBACK.EMP_ID,
+                                       sandbox.INTERVIEW_FEEDBACK.IFB_FEEDBACK,
+                                       sandbox.INTERVIEW_FEEDBACK.IFB_CREATED_AT,
+                                       sandbox.INTERVIEW_FEEDBACK.IFB_UPDATED_AT)
+VALUES (1,1, 1, 'The best candidate, etc.....', now(), now());
