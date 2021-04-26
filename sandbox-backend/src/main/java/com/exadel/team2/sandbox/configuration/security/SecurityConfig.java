1516fb57a61aa47876f28923d190fb0348c82504
Russell Skalden <russell.skalden@gmail.com>
2021-04-26 23:53:13 +0500

    Minor fixes

--- sandbox-backend/src/main/java/com/exadel/team2/sandbox/configuration/security/SecurityConfig.java
+++ sandbox-backend/src/main/java/com/exadel/team2/sandbox/configuration/security/SecurityConfig.java
@@ -1,18 +1,17 @@
 package com.exadel.team2.sandbox.configuration.security;
 
 import com.exadel.team2.sandbox.service.EmployeeService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -21,10 +20,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final EmployeeService employeeService;
     private final JwtTokenFilter jwtTokenFilter;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public SecurityConfig(EmployeeService employeeService, JwtTokenFilter jwtTokenFilter) {
+    public SecurityConfig(EmployeeService employeeService,
+                          JwtTokenFilter jwtTokenFilter, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.employeeService = employeeService;
         this.jwtTokenFilter = jwtTokenFilter;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 
         // Inherit security context in async function calls
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
@@ -32,13 +34,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(employeeService);
+        auth.userDetailsService(employeeService).passwordEncoder(bCryptPasswordEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-        // Disable CSRF
+        // Disable CSRF verification
                 .csrf().disable()
         // Set session management to stateless
                 .sessionManagement()
@@ -51,7 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/swagger-resources/**").permitAll()
                 // Our public endpoints
                     .antMatchers("/auth/login").permitAll()
-                // Our private endpoints
+                // Our private endpoints loading from DB
                     .antMatchers(HttpMethod.GET,"/roles").hasAuthority("admin_view")
                     .anyRequest().authenticated();
 
--- sandbox-backend/src/main/java/com/exadel/team2/sandbox/configuration/security/SecurityConstants.java
+++ sandbox-backend/src/main/java/com/exadel/team2/sandbox/configuration/security/SecurityConstants.java
@@ -6,5 +6,4 @@ public final class SecurityConstants {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String TOKEN_ISSUER = "ExadelTeam2BackEnd";
     public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/api/admin/login";
 }
