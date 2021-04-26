a7f4d66b29818c10ed81e6d5c3e9019098891acb
SaranchukVadym <fksa2.saranchykvadum@gmial.com>
2021-04-26 17:32:14 +0300

    modify candidate table

--- sandbox-backend/src/main/java/com/exadel/team2/sandbox/security/jwt/JwtCandidateDetails.java
+++
@@ -1,9 +0,0 @@
-package com.exadel.team2.sandbox.security.jwt;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface JwtCandidateDetails extends UserDetailsService {
-    JwtCandidate loadCandidateById(Long id);
-}
