7385dea71889f69bce07a5808d8fd5bbf0c3b290
akarzhaneuski <akarzhaneuski@exadel.com>
2021-05-04 20:38:24 +0300

    Fixed issue with spring boot tests

--- sandbox-web/src/test/java/com/exadel/team2/sandbox/BaseTestClass.java
+++ sandbox-web/src/test/java/com/exadel/team2/sandbox/BaseTestClass.java
@@ -3,8 +3,16 @@ package com.exadel.team2.sandbox;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest
+@TestPropertySource(properties =
+        {
+                "spring.liquibase.enabled=false",
+                "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration, " +
+                        "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration, " +
+                        "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+        }
+)
 public class BaseTestClass {
 }
