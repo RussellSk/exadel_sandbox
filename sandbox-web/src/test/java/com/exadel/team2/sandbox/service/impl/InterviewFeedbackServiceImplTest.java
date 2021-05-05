2776470ad9f4d86f25270630cde410f062b10f35
Aliaksei Halkin <pochtax3@gmail.com>
2021-05-05 23:20:08 +0300

    added SAVE in table CandidateEvent

--- sandbox-web/src/test/java/com/exadel/team2/sandbox/service/impl/InterviewFeedbackServiceImplTest.java
+++ sandbox-web/src/test/java/com/exadel/team2/sandbox/service/impl/InterviewFeedbackServiceImplTest.java
@@ -1,13 +1,20 @@
 package com.exadel.team2.sandbox.service.impl;
 
 import com.exadel.team2.sandbox.BaseTestClass;
+import com.exadel.team2.sandbox.dao.CandidateDAO;
+import com.exadel.team2.sandbox.dao.EmployeeDAO;
 import com.exadel.team2.sandbox.dao.InterviewFeedbackDAO;
+import com.exadel.team2.sandbox.entity.CandidateEntity;
+import com.exadel.team2.sandbox.entity.EmployeeEntity;
 import com.exadel.team2.sandbox.entity.InterviewFeedbackEntity;
+import com.exadel.team2.sandbox.mapper.InterviewFeedbackMapper;
 import com.exadel.team2.sandbox.service.InterviewFeedbackService;
+import com.exadel.team2.sandbox.web.interview_feedback.CreateInterviewFeedbackDto;
 import com.exadel.team2.sandbox.web.interview_feedback.ResponseInterviewFeedbackDto;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -15,90 +22,127 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@SpringBootTest(classes = {InterviewFeedbackServiceImpl.class, InterviewFeedbackMapper.class})
 class InterviewFeedbackServiceImplTest extends BaseTestClass {
-    private static final long ID = 1L;
-    @MockBean
-    private InterviewFeedbackDAO dao;
+
+    private static final Long IFB_ID = 1L;
+    private static final Long ID = 2L;
+
     @Autowired
-    private InterviewFeedbackService service;
+    private InterviewFeedbackService interviewFeedbackService;
+
+    @MockBean
+    private EmployeeDAO employeeDAO;
+
+    @MockBean
+    private CandidateDAO candidateDAO;
+
+    @MockBean
+    private InterviewFeedbackDAO interviewFeedbackDAO;
+// TODO: 05.05.2021 I need to decide a problem with LocalDateTime.now()
+//    @Test
+//    void saveWhenIFB_NotNull() {
+//        InterviewFeedbackEntity interviewFeedbackEntity = createOptional().get();
+//        interviewFeedbackEntity.setId(null);
+//        when(candidateDAO.findById(ID)).thenReturn(Optional.of(interviewFeedbackEntity.getCandidate()));
+//        when(employeeDAO.findById(ID)).thenReturn(Optional.of(interviewFeedbackEntity.getEmployee()));
+//        when(interviewFeedbackDAO.save(interviewFeedbackEntity)).thenReturn(interviewFeedbackEntity);
+//        ResponseInterviewFeedbackDto responseInterviewFeedbackDto = interviewFeedbackService.save(createIFB_DTO());
+//        assertNotNull(responseInterviewFeedbackDto);
+//        verify(candidateDAO, times(1)).findById(ID);
+//        verify(employeeDAO, times(1)).findById(ID);
+//        verify(interviewFeedbackDAO, times(1)).save(interviewFeedbackEntity);
+//    }
 
     @Test
-    void getByIdExist() {
-        when(dao.findById(ID)).thenReturn(getInterwievFeedbackEntity());
-        ResponseInterviewFeedbackDto actualInterwievFeedback = service.getById(ID);
-        Assertions.assertEquals(ID, actualInterwievFeedback.getId());
+    void save_ShouldThrowException_WhenInterviewFeedback_IsNull() {
+        when(interviewFeedbackDAO.save(null)).thenReturn(null);
+        assertThrows(Exception.class, () -> interviewFeedbackService.save(null));
     }
 
     @Test
-    void getById_noInterviewFeedback_exceptionThrown() {
-        when(dao.findById(ID)).thenReturn(Optional.empty());
-        assertThrows(ResponseStatusException.class, () -> service.getById(ID));
+    void findById_ShouldFindInterviewFeedback_WhenGivenExistingId() {
+        when(interviewFeedbackDAO.findById(ArgumentMatchers.eq(IFB_ID))).thenReturn(createOptional());
+        ResponseInterviewFeedbackDto resp = interviewFeedbackService.getById(IFB_ID);
+        assertNotNull(resp);
+        assertEquals(IFB_ID, resp.getId());
+        verify(interviewFeedbackDAO, times(1)).findById(ArgumentMatchers.eq(IFB_ID));
     }
 
     @Test
-    void getAllInterviewExistInDB() {
-        List<InterviewFeedbackEntity> interviewFeedbackEntities = createInterviewFeedbackEntity();
-        when(dao.findAll()).thenReturn(interviewFeedbackEntities);
-        List<ResponseInterviewFeedbackDto> actual = service.getAll();
-        List<ResponseInterviewFeedbackDto> expected = createInterviewFeedbackDto();
-        Assertions.assertEquals(expected, actual);
+    void findById_ShouldThrowException_WhenGivenNotExistingId() {
+        when(interviewFeedbackDAO.findById(ArgumentMatchers.eq(IFB_ID))).thenReturn(Optional.empty());
+        assertThrows(ResponseStatusException.class, () -> interviewFeedbackService.getById(IFB_ID));
+        verify(interviewFeedbackDAO, times(1)).findById(ArgumentMatchers.eq(IFB_ID));
     }
 
     @Test
-    void getAll_IFB_DoesNotExist_exceptionThrown() {
-        List<InterviewFeedbackEntity> emptyEntity = createEmptyEntity();
-        when(dao.findAll()).thenReturn(emptyEntity);
-        assertThrows(ResponseStatusException.class, () -> service.getAll());
+    void findAll_ShouldFindAll_WhenInterviewFeedback_ExistingInDb() {
+        when(interviewFeedbackDAO.findAll()).thenReturn(createIFB_List());
+        List<ResponseInterviewFeedbackDto> dtoList = interviewFeedbackService.getAll();
+        assertNotNull(dtoList);
+        assertEquals(createResponseIFB_DTO_List(), dtoList);
+        verify(interviewFeedbackDAO, times(1)).findAll();
     }
 
     @Test
-    void deleteById_IFB_Exists() {
-        when(dao.existsById(ID)).thenReturn(true);
-        boolean status = service.delete(ID);
-        verify(dao).deleteById(ID);
-        Assertions.assertTrue(status);
+    void findAll_ShouldThrowException_WhenInterviewFeedback_NotExistingInDb() {
+        when(interviewFeedbackDAO.findAll()).thenReturn(createEmptyIFB_List());
+        assertThrows(ResponseStatusException.class, () -> interviewFeedbackService.getAll());
     }
 
-    private List<InterviewFeedbackEntity> createInterviewFeedbackEntity() {
-        InterviewFeedbackEntity entityFirst = new InterviewFeedbackEntity();
-        entityFirst.setId(1L);
-        InterviewFeedbackEntity entitySecond = new InterviewFeedbackEntity();
-        entitySecond.setId(2L);
-        InterviewFeedbackEntity entityThird = new InterviewFeedbackEntity();
-        entityThird.setId(3L);
-        List<InterviewFeedbackEntity> interviewFeedbackEntities = new ArrayList<>();
-        interviewFeedbackEntities.add(entityFirst);
-        interviewFeedbackEntities.add(entitySecond);
-        interviewFeedbackEntities.add(entityThird);
-        return interviewFeedbackEntities;
+    @Test
+    void deleteById_ShouldDeleteInterviewFeedback_ById_WhenGivenExistingId() {
+        when(interviewFeedbackDAO.existsById(IFB_ID)).thenReturn(true);
+        interviewFeedbackService.delete(IFB_ID);
+        verify(interviewFeedbackDAO, times(1)).deleteById(IFB_ID);
     }
 
-    private List<ResponseInterviewFeedbackDto> createInterviewFeedbackDto() {
-        ResponseInterviewFeedbackDto responseInterviewFeedbackDto1 = new ResponseInterviewFeedbackDto();
-        responseInterviewFeedbackDto1.setId(1L);
-        ResponseInterviewFeedbackDto responseInterviewFeedbackDto2 = new ResponseInterviewFeedbackDto();
-        responseInterviewFeedbackDto2.setId(2L);
-        ResponseInterviewFeedbackDto responseInterviewFeedbackDto3 = new ResponseInterviewFeedbackDto();
-        responseInterviewFeedbackDto3.setId(3L);
-        List<ResponseInterviewFeedbackDto> responseInterviewFeedbackDtos = new ArrayList<>();
-        responseInterviewFeedbackDtos.add(responseInterviewFeedbackDto1);
-        responseInterviewFeedbackDtos.add(responseInterviewFeedbackDto2);
-        responseInterviewFeedbackDtos.add(responseInterviewFeedbackDto3);
-        return responseInterviewFeedbackDtos;
+    @Test
+    void deleteById_ShouldThrowException_WhenGivenNotExistingId() {
+        when(interviewFeedbackDAO.existsById(IFB_ID)).thenReturn(false);
+        assertThrows(Exception.class, () -> interviewFeedbackService.delete(IFB_ID));
     }
 
-    private List<InterviewFeedbackEntity> createEmptyEntity() {
-        List<InterviewFeedbackEntity> entities = new ArrayList<>();
-        return entities;
+    private List<InterviewFeedbackEntity> createIFB_List() {
+        InterviewFeedbackEntity interviewFeedbackEntity = new InterviewFeedbackEntity();
+        interviewFeedbackEntity.setId(IFB_ID);
+        List<InterviewFeedbackEntity> list = new ArrayList<>();
+        list.add(interviewFeedbackEntity);
+        return list;
     }
 
-    private Optional<InterviewFeedbackEntity> getInterwievFeedbackEntity() {
+    private List<InterviewFeedbackEntity> createEmptyIFB_List() {
+        return new ArrayList<>();
+    }
+
+    private List<ResponseInterviewFeedbackDto> createResponseIFB_DTO_List() {
+        ResponseInterviewFeedbackDto responseInterviewFeedbackDto = new ResponseInterviewFeedbackDto();
+        responseInterviewFeedbackDto.setId(IFB_ID);
+        List<ResponseInterviewFeedbackDto> list = new ArrayList<>();
+        list.add(responseInterviewFeedbackDto);
+        return list;
+    }
+
+    private CreateInterviewFeedbackDto createIFB_DTO() {
+        CreateInterviewFeedbackDto createInterviewFeedbackDto = new CreateInterviewFeedbackDto();
+        createInterviewFeedbackDto.setIdCandidate(ID);
+        createInterviewFeedbackDto.setIdEmployee(ID);
+        return createInterviewFeedbackDto;
+    }
+
+    private Optional<InterviewFeedbackEntity> createOptional() {
+        CandidateEntity candidateEntity = new CandidateEntity();
+        candidateEntity.setId(ID);
+        EmployeeEntity employeeEntity = new EmployeeEntity();
+        employeeEntity.setId(ID);
         InterviewFeedbackEntity entity = new InterviewFeedbackEntity();
-        entity.setId(ID);
+        entity.setId(IFB_ID);
+        entity.setEmployee(employeeEntity);
+        entity.setCandidate(candidateEntity);
         return Optional.of(entity);
     }
 }
