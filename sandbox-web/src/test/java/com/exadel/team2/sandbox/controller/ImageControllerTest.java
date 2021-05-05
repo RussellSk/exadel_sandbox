013a77220b6b7908d7ca170000b9b28599e42aa8
Vadim Blagiy <vadblagiy@gmail.com>
2021-05-05 11:45:35 +0300

    test for controller

--- sandbox-web/src/test/java/com/exadel/team2/sandbox/controller/ImageControllerTest.java
+++ sandbox-web/src/test/java/com/exadel/team2/sandbox/controller/ImageControllerTest.java
@@ -1,27 +1,68 @@
 package com.exadel.team2.sandbox.controller;
 
-import com.exadel.team2.sandbox.mapper.Mapper;
 import com.exadel.team2.sandbox.service.ImageService;
+import com.exadel.team2.sandbox.web.image.ImageResponseDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class ImageControllerTest {
 
     MockMvc mockMvc;
-    Mapper mapper;
+    ObjectMapper objectMapper;
     ImageService imageService;
 
     @BeforeEach
     void setUp() {
         imageService = mock(ImageService.class);
-//        mapper = new Mapper();
+        mockMvc = MockMvcBuilders.standaloneSetup(new ImageController(imageService)).build();
+        objectMapper = new ObjectMapper();
     }
 
     @Test
-    void getImageById() {
+    void save_imagesInDB_ok() throws Exception {
+        when(imageService.save(any())).thenReturn(responseDto());
+
+        mockMvc.perform(post("/image")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(responseDto())))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(responseDto())));
     }
+
+
+    private ImageResponseDTO responseDto() {
+        ImageResponseDTO imageDTO = new ImageResponseDTO();
+        imageDTO.setId(5L);
+        imageDTO.setImageName("Java cover");
+        imageDTO.setAltText("Short description about image");
+        imageDTO.setExt("jpg");
+        imageDTO.setPath("local path");
+        imageDTO.setSize(5);
+
+        return imageDTO;
+    }
+
+//    private ImageEntity saveImageEntity() {
+//        ImageEntity imageEntity = new ImageEntity();
+//        imageEntity.setImageName("Java cover");
+//        imageEntity.setAltText("Short description about image");
+//        imageEntity.setExt("jpg");
+//        imageEntity.setPath("local path");
+//        imageEntity.setSize(5);
+//
+//        return imageEntity;
+//    }
 }
