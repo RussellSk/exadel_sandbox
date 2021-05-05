04ba660ea5d8e624a4f0a3155d5645b069297596
Russell Skalden <russell.skalden@gmail.com>
2021-05-05 23:14:46 +0500

    Create table EMPLOYEE_AVAILABILITY_TIME and add create, delete, get features in new EmployeeAvailabilityTimeService

---
+++ sandbox-backend/src/main/java/com/exadel/team2/sandbox/service/EmployeeAvailabilityTimeService.java
@@ -0,0 +1,10 @@
+package com.exadel.team2.sandbox.service;
+
+import com.exadel.team2.sandbox.web.employee_availability_time.CreateEmployeeAvailabilityTimeDto;
+import com.exadel.team2.sandbox.web.employee_availability_time.ResponseEmployeeAvailabilityTimeDto;
+import com.exadel.team2.sandbox.web.employee_availability_time.UpdateEmployeeAvailabilityTimeDto;
+
+public interface EmployeeAvailabilityTimeService extends GeneralService<ResponseEmployeeAvailabilityTimeDto,
+        CreateEmployeeAvailabilityTimeDto,
+        UpdateEmployeeAvailabilityTimeDto> {
+}
