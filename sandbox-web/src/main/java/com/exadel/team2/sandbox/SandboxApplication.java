b1edfcaaab68ff03adabd40722e4c628c56a80bf
Russell Skalden <russell.skalden@gmail.com>
2021-05-13 17:33:38 +0500

    Remove unused import in SandboxApplication.java

--- sandbox-web/src/main/java/com/exadel/team2/sandbox/SandboxApplication.java
+++ sandbox-web/src/main/java/com/exadel/team2/sandbox/SandboxApplication.java
@@ -1,19 +1,13 @@
 package com.exadel.team2.sandbox;
 
 import com.exadel.team2.sandbox.configuration.FileStorageProperties;
-import com.exadel.team2.sandbox.configuration.security.JwtTokenFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
-        FileStorageProperties.class,
+        FileStorageProperties.class
 })
 public class SandboxApplication {
     public static void main(String[] args) {
